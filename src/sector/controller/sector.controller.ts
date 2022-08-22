eda6bc61fc1edea5c8dba15040c27340914641f1
santoskarolina <anna15soliveira@gmail.com>
2022-08-22 10:24:58 -0300

    Listar setores sem permissão

--- src/sector/controller/sector.controller.ts
+++ src/sector/controller/sector.controller.ts
@@ -1,20 +1,20 @@
-import {Body, Controller, Get, Post, UseGuards} from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { SectorService } from '../services/sector.service';
-import {SectorCreate} from "../entities/dto/sector.dto";
-import {JwtLocalGuard} from "../../auth/guards/jwt.guard";
+import { SectorCreate } from '../entities/dto/sector.dto';
+import { JwtLocalGuard } from '../../auth/guards/jwt.guard';
 
-@UseGuards(JwtLocalGuard)
 @Controller('sector')
 export class SectorController {
   constructor(private readonly sectorService: SectorService) {}
 
   @Post()
-  create(@Body() sector: SectorCreate){
-    return this.sectorService.create(sector)
+  @UseGuards(JwtLocalGuard)
+  create(@Body() sector: SectorCreate) {
+    return this.sectorService.create(sector);
   }
 
   @Get()
-  findAll(){
-    return this.sectorService.findAll()
+  findAll() {
+    return this.sectorService.findAll();
   }
 }
