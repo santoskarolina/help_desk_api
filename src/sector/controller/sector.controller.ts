46031d12ab0a0dff543ea238cf48722e3c4261ed
santoskarolina <anna15soliveira@gmail.com>
2022-08-23 18:39:42 -0300

    remove guard from sector route

--- src/sector/controller/sector.controller.ts
+++ src/sector/controller/sector.controller.ts
@@ -1,14 +1,12 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SectorService } from '../services/sector.service';
 import { SectorCreate } from '../entities/dto/sector.dto';
-import { JwtLocalGuard } from '../../auth/guards/jwt.guard';
 
 @Controller('sector')
 export class SectorController {
   constructor(private readonly sectorService: SectorService) {}
 
   @Post()
-  @UseGuards(JwtLocalGuard)
   create(@Body() sector: SectorCreate) {
     return this.sectorService.create(sector);
   }
