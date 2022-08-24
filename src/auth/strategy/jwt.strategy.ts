1f1cac7e79ac0c4a13603de050bfbfd4f86df6e4
santoskarolina <anna15soliveira@gmail.com>
2022-08-24 11:50:15 -0300

    update: eslint-disabl

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,19 +1,20 @@
+/* eslint-disable prettier/prettier */
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import {jwtConstants} from "../constanta";
-import {Injectable} from "@nestjs/common";
+import { jwtConstants } from '../constanta';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor() {
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: false,
-            secretOrKey: jwtConstants.secret,
-        });
-    }
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
+    });
+  }
 
-    async validate(payload: any) {
-        return { id: payload.sub, email: payload.email };
-    }
+  async validate(payload: any) {
+    return { id: payload.sub, email: payload.email };
+  }
 }
